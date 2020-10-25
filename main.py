c40141e202d23a09786301b6cc6ca54dd2d134de
Yukun Yang <yukunyang@ucsb.edu>
2020-10-25 22:28:12 +0800

    datasets

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import torch
 from network_parser import parse
+from datasets import loadMNIST, loadXOR
 import cnns
 import argparse
 
@@ -7,3 +8,24 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-config', action='store', dest='config',\
             help='The path of config file')
+    parser.add_argument('-checkpoint', action='store', dest='checkpoint',\
+            help='The path of checkpoint, if use checkpoint')
+    try:
+        args = parser.parse_args()
+    except:
+        parser.print_help()
+        exit(0)
+    if args.config is None:
+        raise Exception('Unrecognized config file.')
+    else:
+        config_path = args.config
+    logging.basicConfig(filename='result.log', level=logging.INFO)
+    logging.info("start parsing settings")
+    params = parse(config_path)
+    logging.info("finish parsing settings")
+    glv.init(params)
+    logging.info("dataset loaded")
+    if params['Network']['dataset'] == "MNIST":
+        data_path = os.path.expanduser(params['Network']['data_path'])
+        train_loader, test_loader = loadMNIST.get_mnist(data_path,\
+                params['Network'])
