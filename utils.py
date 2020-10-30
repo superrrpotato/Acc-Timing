8bacf2f4874ca03f3366e3acaa3f3b2c1999da86
Yukun Yang <yukunyang@ucsb.edu>
2020-10-30 21:23:19 +0800

    fix space

--- utils.py
+++ utils.py
@@ -103,16 +103,18 @@ class learningStats():
 
 class aboutCudaDevices():
      def __init__(self):
-         pass
+        pass
      def info(self):
-         """Class representation as number of devices connected and about
-         them."""
-         num = cuda.Device.count()
-         string = ""
-         string += ("%d device(s) found:\n" % num)
-         for i in range(num):
-             string += ("    %d) %s (Id: %d)\n" % ((i + 1),\
-                 cuda.Device(i).name(), i))
-             string += ("          Memory: %.2f GB\n" %\
-                     (cuda.Device(i).total_memory() / 1e9))
-             return string
+        """
+        Class representation as number of devices connected and about
+        them.
+        """
+        num = cuda.Device.count()
+        string = ""
+        string += ("%d device(s) found:\n" % num)
+        for i in range(num):
+            string += ("    %d) %s (Id: %d)\n" % ((i + 1),\
+                cuda.Device(i).name(), i))
+            string += ("          Memory: %.2f GB\n" %\
+                    (cuda.Device(i).total_memory() / 1e9))
+            return string
